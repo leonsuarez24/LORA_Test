3c5e07cb666bc5d1c9f2ede222bf347a6766ecd7
leonsuarez24 <leonsuarez24@gmail.com>
2024-07-17 16:07:07 -0500

    update network

--- networks.py
+++ networks.py
@@ -5,13 +5,20 @@ class DemoNet(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=2, kernel_size=3, padding=1)
-        self.linear = nn.Linear(392, 10)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=100, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=100, out_channels=100, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=100, out_channels=1, kernel_size=3, padding=1)
+        self.linear = nn.Linear(49, 10)
 
     def forward(self, x):
         h = self.conv1(x)
         h = F.relu(h)
         h = F.max_pool2d(h, 2)
-        h = h.view(-1, 392)
+        h = self.conv2(h)
+        h = F.relu(h)
+        h = F.max_pool2d(h, 2)
+        h = self.conv3(h)
+        h = F.relu(h)
+        h = h.view(-1, 49)
         h = self.linear(h)
         return h
